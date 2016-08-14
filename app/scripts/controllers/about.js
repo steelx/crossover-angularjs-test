15de4c0a9c3f91932cc909dd2f9e67e3f12f395d
Steel <ajinkya@riva.co>
2016-08-14 16:11:53 +0530

    crossover test app Ajinkya Borade

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crossoverTestAjinkyaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crossoverTestAjinkyaApp
+ */
+angular.module('crossoverTestAjinkyaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crossoverTestAjinkyaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crossoverTestAjinkyaApp
+ */
+angular.module('crossoverTestAjinkyaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
